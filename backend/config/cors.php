e74ccb1e978cbdfec54621f53b0014cb8abcf106
Yaddehi Chathura Umayanga Sriyarathna <info.sloption@gmail.com>
2026-05-13 06:23:20 +0530

    add ec2 ip to cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -21,6 +21,7 @@ return [
         'http://localhost:5173',
         'http://127.0.0.1:3000',
         'http://127.0.0.1:5173',
+        '3.83.53.113:3000'
     ],
 
     'allowed_origins_patterns' => [],
